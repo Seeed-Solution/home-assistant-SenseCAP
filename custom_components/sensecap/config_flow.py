87ad6333f0ddf11eeabb51fef67107f18452159f
JoJang2u <47235085+QingWind6@users.noreply.github.com>
2023-10-17 14:33:21 +0800

    Update config_flow.py

--- custom_components/sensecap/config_flow.py
+++ custom_components/sensecap/config_flow.py
@@ -1,50 +1,48 @@
-from __future__ import annotations
-
+# from copy import deepcopy
 import logging
-from typing import Any
+from typing import Any, Dict, Optional
+
+from homeassistant import config_entries, core
+from homeassistant.const import CONF_ACCESS_TOKEN,CONF_USERNAME,CONF_PASSWORD,CONF_HOST,CONF_URL
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
 
 import voluptuous as vol
 
-from homeassistant import config_entries, exceptions
-from homeassistant.core import HomeAssistant
-
-from homeassistant.const import CONF_USERNAME,CONF_PASSWORD
-import homeassistant.helpers.config_validation as cv
-
-from .const import (
-    DOMAIN,
-    CONF_COMMAND_TOPIC,
-)
+from .const import CONF_COMMAND_TOPIC, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema({("command_topic"): str},
+AUTH_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_COMMAND_TOPIC, default="application/#"): cv.string,
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_USERNAME): cv.string, 
+        vol.Required(CONF_PASSWORD): cv.string,
+    }
 )
-
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     topic: str  # 添加 topic 属性
-
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
-    # data: Optional[Dict[str, Any]]
-
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
             try:
                 mqtt_topic = user_input[CONF_COMMAND_TOPIC]
+                mqtt_host = user_input[CONF_HOST]
                 user_name = user_input.get(CONF_USERNAME)
                 user_passwd = user_input.get(CONF_PASSWORD)
 
                 data = {
                     CONF_COMMAND_TOPIC: mqtt_topic,
+                    CONF_HOST: mqtt_host,
                     CONF_USERNAME: user_name,
                     CONF_PASSWORD: user_passwd,
                 }
 
-                return self.async_create_entry(title=mqtt_topic, data=data)
+                return self.async_create_entry(title="SenseCAP", data=data)
 
             except Exception as e:
                 # 处理异常，可以记录日志或者返回适当的错误信息给用户
@@ -53,12 +51,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_COMMAND_TOPIC, default="application/#"): str,
-                    vol.Optional(CONF_USERNAME): str,
-                    vol.Optional(CONF_PASSWORD): str,
-                }
-            ),
+            data_schema=AUTH_SCHEMA,
             errors=errors or {},
         )
