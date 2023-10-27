e515a3715a317e2c6500a0cf83771223bc48395a
JoJang2u <47235085+QingWind6@users.noreply.github.com>
2023-10-27 11:41:47 +0800

    Update __init__.py
    Fix : mqtt client stop

--- custom_components/sensecap/__init__.py
+++ custom_components/sensecap/__init__.py
@@ -15,33 +15,18 @@ _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[str] = ["sensor"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    mqtt_topic = entry.data["command_topic"]
-    host = entry.data["host"]
-    user_name = entry.data["username"]
-    user_passwd = entry.data["password"]
-
-
-    hass.data.setdefault(DOMAIN, {})["mqtt_topic"] = mqtt_topic
-    hass.data.setdefault(DOMAIN, {})["user_name"] = user_name
-    hass.data.setdefault(DOMAIN, {})["user_passwd"] = user_passwd
-    hass.data.setdefault(DOMAIN, {})["mqtt_host"] = host
-
-    hass.data.setdefault(DOMAIN, {})["dev_eui"] = []
-    hass.data.setdefault(DOMAIN, {})["entities"] = {}
-
-
+    hass.data.setdefault(DOMAIN, {})
+    data = dict(entry.data)
+    hass.data[DOMAIN][entry.entry_id] = data
+    hass.data[DOMAIN]["mqtt_client"] = 1
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
-        hass.data[DOMAIN].pop("mqtt_topic", None)
-        hass.data[DOMAIN].pop("user_name", None)
-        hass.data[DOMAIN].pop("user_passwd", None)
-        hass.data[DOMAIN].pop("mqtt_host", None)
-        hass.data[DOMAIN].pop("dev_eui", None)
-        hass.data[DOMAIN].pop("entities", None)
+        hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN]["mqtt_client"] = 0
 
     return unload_ok
-
