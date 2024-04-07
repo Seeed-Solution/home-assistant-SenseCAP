e5967672c7c5532e77fae67c2096f2b64f4adeb2
Jesse R Codling <codling@umich.edu>
2024-04-07 11:54:00 -0400

    Handle payloads from The Things Stack
    puts all of the `process_mqtt_payload`-like calls into one function, and
    gives it a switch for pulling the `messages` array from uplink messages
    in The Things Stack v3 format.
    
    Includes:
    fixup! still return None for unparseable payloads

--- custom_components/sensecap/hub.py
+++ custom_components/sensecap/hub.py
@@ -13,18 +13,37 @@ from .const import (
 
 _LOGGER = logging.getLogger(__name__)
 
-
-def process_mqtt_payload(payload):
+def process_chirpstack(payload):
     if "deviceInfo" and "object" not in payload:
         return [],[]
 
-    dev_eui = payload["deviceInfo"]["devEui"]
+    eui = payload["deviceInfo"]["devEui"]
+    msgs = payload["object"]["messages"]
 
-    if len(payload["object"]["messages"]) > 1:
-        messages = payload["object"]["messages"]
+    return eui, msgs
+
+def process_ttn(payload):
+    eui = payload["end_device_ids"]["dev_eui"]
+    msgs = payload["uplink_message"]["decoded_payload"]["messages"]
+
+    return eui, msgs
+
+
+
+
+def process_mqtt_payload(payload):
+    if "deviceInfo" and "object" in payload:
+        dev_eui, raw_msgs = process_chirpstack(payload)
+    elif "uplink_message" and "end_device_ids" in payload:
+        dev_eui, raw_msgs = process_ttn(payload)
+    else:
+        return None, None
+
+    if len(raw_msgs) > 1:
+        messages = raw_msgs
         dev_messages = [messages]
-    elif len(payload["object"]["messages"]) == 1:
-        dev_messages = payload["object"]["messages"]
+    elif len(raw_msgs) == 1:
+        dev_messages = raw_msgs
     else:
         dev_messages = []
 
